511312c4f50bfaaa092db04a8da7c07c86c424d2
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-04-21 14:30:10 -0300

    global var and Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import format from 'date-fns/format';
+
+import ptBr from 'date-fns/locale/pt-BR';
+
+
+import styles from './styles.module.scss'
+
+export function Header() {
+
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBr,
+  });
+
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg"></img>
+      <p>O melhor para voce ouvir, sempre</p>
+      <span>{currentDate}</span>
+    </header>
+  )
+}
