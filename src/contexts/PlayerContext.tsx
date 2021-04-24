d5f0f5f798809d1fe85fd2215895d60a6d0e323e
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-04-24 13:32:51 -0300

    playercontext refactor

---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,53 @@
+import { createContext, ReactNode, useState } from 'react'
+
+
+type Episode = {
+  title: string;
+  members: string;
+  thumbnail: string;
+  duration: number;
+  url: string;
+}
+
+type playerContextData = {
+  episodelist: Episode[];
+  currentEpisodeIndex: number;
+  isPlaying: boolean;
+  togglePlay: () => void;
+  play: (episode: Episode) => void;
+  setPlayingState: (state: boolean) => void;
+}
+
+type PlayerContextProviderProps = {
+  children: ReactNode;
+}
+
+export const PlayerContext = createContext({} as playerContextData);
+
+export function PlayerContextProvider({ children }: PlayerContextProviderProps) {
+
+  const [episodelist, setEpisodeList] = useState([])
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode: Episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
+  return (
+
+    <PlayerContext.Provider value={{ episodelist, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
+      {children}
+    </PlayerContext.Provider>
+  )
+}
