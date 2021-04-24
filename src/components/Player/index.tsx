760fe3582b8438dcf06af4541be3bc4ffc83a1a6
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-04-24 14:36:39 -0300

    player podcast time

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,18 +1,28 @@
-import { useContext, useEffect, useRef } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import { usePlayer } from '../../contexts/PlayerContext'
 import styles from './styles.module.scss'
 import Image from 'next/image'
 import Slider from 'rc-slider' 
 import 'rc-slider/assets/index.css'
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString'
 
 export function Player() {
 
   const audioRef = useRef<HTMLAudioElement>(null);
 
+  const [progress, setProgress] = useState(0);
+
   const {episodelist, currentEpisodeIndex, isPlaying, togglePlay, setPlayingState, playNext, playPrevious, hasNext, hasPrevious, toggleLoop, isLooping, toggleShuffle, isShuffling} = usePlayer()
 
   const episode = episodelist[currentEpisodeIndex]
 
+  function setupProgressListener() {
+    audioRef.current.currentTime = 0;
+
+    audioRef.current.addEventListener('timeupdate', () => {
+      setProgress(Math.floor(audioRef.current.currentTime));
+    });
+  }
 
   useEffect(() => {
 
@@ -49,7 +59,7 @@ export function Player() {
 
       <footer className={!episode ? styles.empty : ''}>
         <div className={styles.progress}>
-          <span>00:00</span>
+          <span>{convertDurationToTimeString(progress)}</span>
           <div className={styles.slider}>
             {episode ? (
               <Slider trackStyle={{backgroundColor: '#84d361'}} railStyle={{backgroundColor: '#9f775ff'}} handleStyle={{borderColor: '#84d361', borderWidth: 4} }  />
@@ -57,12 +67,12 @@ export function Player() {
               <div className={styles.emptySlider}/>
             )}
           </div>
-          <span>00:00</span>
+          <span>{convertDurationToTimeString(episode?.duration ?? 0)}</span>
         </div>
 
         { episode && (
 
-          <audio src={episode.url} ref={audioRef} onPlay={() => setPlayingState(true)} onPause={() => setPlayingState(false)} autoPlay loop={isLooping} />
+          <audio src={episode.url} ref={audioRef} onPlay={() => setPlayingState(true)} onPause={() => setPlayingState(false)} autoPlay loop={isLooping} onLoadedMetadata={setupProgressListener} />
 
         )}
 
